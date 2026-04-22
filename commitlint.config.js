e582f9697bd26e098bc125aae7ebb298ba1fc7a6
Michael Caines <michael.caines@nscc.ca>
2026-04-22 18:29:14 +0000

    fix: update commit message validation and improve pre-commit checks

--- commitlint.config.js
+++ commitlint.config.js
@@ -20,9 +20,10 @@ module.exports = {
         'revert', // reverts a previous commit
       ],
     ],
-    'subject-case': [2, 'always', 'lower-case'],
+    'subject-case': [0],
     'subject-empty': [2, 'never'],
     'subject-full-stop': [2, 'never', '.'],
     'header-max-length': [2, 'always', 100],
+    'body-max-line-length': [2, 'always', 200],
   },
 };
